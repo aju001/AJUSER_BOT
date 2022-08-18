a2c6a45e2958686153eac84c15564630c6d056fa
Aju <94348613+aju001@users.noreply.github.com>
2022-08-18 16:20:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,18 +10,18 @@ logging.getLogger("imdbpy").setLevel(logging.ERROR)
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from database.autofilter_mdb import Media
-from LuciferMoringstar_Robot import API_ID, API_HASH, BOT_TOKEN, temp
+from Ajuser_Bot import API_ID, API_HASH, BOT_TOKEN, temp
 
 class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            "LuciferMoringstar_Robot",
+            "Ajuser_Bot",
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
             workers=50,
-            plugins={"root": "LuciferMoringstar_Robot"},
+            plugins={"root": "Ajuser_Bot"},
             sleep_threshold=5,
         )
 
